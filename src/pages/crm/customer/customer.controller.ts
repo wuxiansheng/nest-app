57adb147f18de696e549311e45a06daa51c88a0b
wuxiansheng <359684121@qq.com>
2019-07-26 02:00:33 +0800

    updata

--- src/pages/crm/customer/customer.controller.ts
+++ src/pages/crm/customer/customer.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Customer } from './customer.entity';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+  constructor(
+    @InjectRepository(Customer)
+    private readonly customerService: CustomerService,
+  ) {}
+  @Get()
+  async findAll(): Promise<Customer[]> {
+    return await this.customerService.findAll();
+  }
+  @Get()
+  async findOne(id: string): Promise<Customer> {
+    return await this.customerService.findOne(id);
+  }
+}
